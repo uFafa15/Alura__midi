bfef44347f764294521d207200f924110070c80b
uFafa15 <fabriciio.lnsousa@gmail.com>
2023-04-14 14:22:50 -0300

    Listas 1 - apenas teclas

--- main.js
+++ main.js
@@ -2,52 +2,27 @@ function tocaPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
-function tocaClap(){
-    document.querySelector('#som_tecla_clap').play();
-}
-
-function tocaTim(){
-    document.querySelector('#som_tecla_tim').play();
-}
-
-function tocaPuff(){
-    document.querySelector('#som_tecla_puff').play();
-}
-
-function tocaSplash(){
-    document.querySelector('#som_tecla_splash').play();
-}
-
-function tocaToim(){
-    document.querySelector('#som_tecla_toim').play();
-}
-
-function tocaPsh(){
-    document.querySelector('#som_tecla_psh').play();
-}
-
-function tocaTic(){
-    document.querySelector('#som_tecla_tic').play();
-}
-
-function tocaTom(){
-    document.querySelector('#som_tecla_tom').play();
-}
 
 document.querySelector('.tecla_pom').onclick = tocaPom;
 
-document.querySelector('.tecla_clap').onclick = tocaClap;
+const listaTeclas = document.querySelectorAll('.tecla');
 
-document.querySelector('.tecla_tim').onclick = tocaTim;
+listaTeclas[0] = tocaPom;
 
-document.querySelector('.tecla_puff').onclick = tocaPuff;
+listaTeclas[1] = tocaClap;
 
-document.querySelector('.tecla_splash').onclick = tocaSplash;
+listaTeclas[2] = tocaTim;
 
-document.querySelector('.tecla_toim').onclick = tocaToim;
+listaTeclas[3] = tocaPuff;
 
-document.querySelector('.tecla_psh').onclick = tocaPsh;
+listaTeclas[4] = tocaSplash;
 
-document.querySelector('.tecla_tic').onclick = tocaTic;
+listaTeclas[5] = tocaToim;
 
-document.querySelector('.tecla_tom').onclick = tocaTom;
+listaTeclas[6] = tocaPsh;
+
+listaTeclas[7] = tocaTic;
+
+listaTeclas[8] = tocaTom;
+
+// dessa forma, ainda utilizaria muitas linhas e teria que criar as funções dos sons
