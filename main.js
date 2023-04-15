8aeee196ceca3d6898f2b27c4effdd3daa0e2a97
uFafa15 <fabriciio.lnsousa@gmail.com>
2023-04-15 00:01:39 -0300

    utilizando a lista e contador(while)

--- main.js
+++ main.js
@@ -7,22 +7,9 @@ document.querySelector('.tecla_pom').onclick = tocaPom;
 
 const listaTeclas = document.querySelectorAll('.tecla');
 
-listaTeclas[0] = tocaPom;
+let contador = 0
 
-listaTeclas[1] = tocaClap;
-
-listaTeclas[2] = tocaTim;
-
-listaTeclas[3] = tocaPuff;
-
-listaTeclas[4] = tocaSplash;
-
-listaTeclas[5] = tocaToim;
-
-listaTeclas[6] = tocaPsh;
-
-listaTeclas[7] = tocaTic;
-
-listaTeclas[8] = tocaTom;
-
-// dessa forma, ainda utilizaria muitas linhas e teria que criar as funções dos sons
+while (contador < listaTeclas.length){
+    listaTeclas[contador].onclick = tocaPom
+    contador = contador + 1
+}
