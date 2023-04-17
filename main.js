7e70346982b11871a1167be9a44bbb8a5703bab7
uFafa15 <fabriciio.lnsousa@gmail.com>
2023-04-17 17:52:17 -0300

    adicionando texto dinâmico para todos os sons

--- main.js
+++ main.js
@@ -7,8 +7,14 @@ const listaTeclas = document.querySelectorAll('.tecla');
 let contador = 0
 
 while (contador < listaTeclas.length){
-    listaTeclas[contador].onclick = function () {
-    tocaSom('#som_tecla_pom');
+    const tecla = listaTeclas[contador]; //evita a repetição de "listaTeclas[contador]"
+    const instrumento = tecla.classList[1]; // faz com que seja dinâmico e indica o segundo nome da classe
+    const idAudio = `#som_${instrumento}`; // insere o texto #som_ + o dinâmico (nome das teclas)
+
+    console.log(idAudio)
+    tecla.onclick = function () {
+    tocaSom(idAudio);
+
     }
     contador = contador + 1
 }
