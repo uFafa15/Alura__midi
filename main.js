6e3cb1233b413942945e09863b9d06054aa47d44
uFafa15 <fabriciio.lnsousa@gmail.com>
2023-04-17 18:03:36 -0300

    otimizando o laço com o 'for'

--- main.js
+++ main.js
@@ -4,9 +4,8 @@ function tocaSom(idElementoAudio){
 
 const listaTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0
-
-while (contador < listaTeclas.length){
+// o for permite que variáveis sejam inseridos como parâmetros dentro de ()
+for (contador = 0; contador < listaTeclas.length; contador ++){
     const tecla = listaTeclas[contador]; //evita a repetição de "listaTeclas[contador]"
     const instrumento = tecla.classList[1]; // faz com que seja dinâmico e indica o segundo nome da classe
     const idAudio = `#som_${instrumento}`; // insere o texto #som_ + o dinâmico (nome das teclas)
@@ -16,5 +15,4 @@ while (contador < listaTeclas.length){
     tocaSom(idAudio);
 
     }
-    contador = contador + 1
 }
